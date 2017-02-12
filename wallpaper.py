e107b0c4a2d8cb6b3f46d0ec735daf7db1a10137
Adam Cwyk <adam.cwyk@gmail.com>
2017-02-13 02:09:24 +1100

    Fixed resolution assumption
    Fixed resolution assumption where incorrect calculations where performed. Separated width and height of image, and compare them separately. Updated logging information to describe reason for deletion and the value of the stated reason,

--- wallpaper.py
+++ wallpaper.py
@@ -13,18 +13,38 @@ def img_size(file_path):
 		return 0
 
 def keep_or_delete(min_file_size, file_size, path):
-	#Extracts the resolution value of the tuple. 1920, 1080 (FHD resolution) turns to 19201080
-	non_tuple_min_file_size = int(''.join(map(str,min_file_size))) #non tuple of the MINIMUM file size
-	non_tuple_file_size = int(''.join(map(str,file_size))) # non tuple of the file size
 	#Deletes the file if it is below the minimum specified resolution.
-	if non_tuple_file_size < non_tuple_min_file_size:
+	
+	#minimum defined values
+	min_width = min_file_size[0]
+	min_height = min_file_size[1]
+	
+	#file values
+	file_width = file_size[0]
+	file_height = file_size[1]
+	
+	#reason codes:
+	height = "HEIGHT"
+	width = "WIDTH"
+	
+	if file_width < min_width:
+		#remove file
 		os.remove(path)
-		delete_log(path)
+		delete_log(path, width, file_width)
+	elif file_height < min_height:
+		#remove file
+		os.remove(path)
+		delete_log(path, height, file_height)
+	else:
+		print(path, " - KEPT")
 
-def delete_log(path, code):
+def delete_log(path, reason, value):
 	log_path = "sanitize.log"
 	log_file = open(log_path, 'a')
-	log_file.write(path + '\n')
+	log_file.write(path)
+	log_file.write(' - Reason: ' + reason)
+	log_file.write(' - Value: ' + str(value))
+	log_file.write('\n')
 	log_file.close()
 	
 if __name__ == "__main__":
@@ -37,16 +57,22 @@ if __name__ == "__main__":
 		print("Working through the Directory...")
 		for name in onlyfiles:
 			count+= 1
+			
+			# extracts the file name from the path
 			filename = str(name)
 			if os.path.isdir(mypath):
 				pathname = os.path.join(mypath, filename)
 			img_file_size = img_size(pathname)
+			
+			#Determins if path is an image.
 			if img_file_size != 0:
+				print(pathname, " - RETURNED 1 | FILE SIZE - ", img_file_size, "\n")
 				keep_or_delete(min_size, img_file_size, pathname)
 			else:
+				print(pathname, " - RETURNED 0")
 				continue
 		print(count, " files scanned")
 		print("Please view sanitize.log for a list of deleted files")
 	except:
-		print('Please pass directory_name, and minimum resolution')
+		print('\nPlease pass directory_name, and minimum resolution')
 		print('Example: wallpaper.py <directory to wallpapers> <width> <height>')
